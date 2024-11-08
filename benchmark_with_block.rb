02a344522e8b6d3089a82dc4aae5cc770a154f29
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-07 22:38:16 -0500

    Implement benchmark method to measure block execution time

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,14 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now # Record the start time
+  yield # Execute the blovk of code passed to the method
+  end_time = Time.now # Record the end time
+  end_time - start_time # Return the time difference (duration) in seconds
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
-long_string = "apple"*100000000
+long_string = "apple"*100000000 # A large string
  
-running_time = benchmark { long_string.reverse }
+running_time = benchmark { long_string.reverse } # Benchmark the reverse method
  
 puts "string.reverse took #{running_time} seconds to run"
